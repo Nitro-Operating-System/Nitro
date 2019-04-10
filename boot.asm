902b3073f21dca84398643661815412565c66b6d
Plaligraphy <evanac21@gmail.com>
2019-04-10 10:53:26 -0700

    Added Clear and Background interrupts

--- boot.asm
+++ boot.asm
@@ -10,15 +10,21 @@ start:
 	mov ax, 07C0h
 	mov ds, ax
 	
+	MOV AX,0600H    ;06 TO SCROLL & 00 FOR FULLJ SCREEN
+    	MOV BH,71H    ;ATTRIBUTE 7 FOR BACKGROUND AND 1 FOR FOREGROUND
+    	MOV CX,0000H    ;STARTING COORDINATES
+   	MOV DX,184FH    ;ENDING COORDINATES
+    	INT 10H       ;FOR VIDEO DISPLAY
+    	MOV AH,4CH    ;RETURN TO DOS MODE
+    	INT 21H
 ; ------------ Print Handler
 	mov si, text_string	
-	call print_string
-	mov si, test_string			
+	call print_string			
 	mov si, msg
 ; ------------ Print Data
 	text_string db 'Nitro', 0x0D, 0x0A
-	test_string db 'By ec621', 0x0D, 0x0A
 	msg db 'This is currently all the OS does...', 0
+
  print_string:			
 	mov ah, 0Eh; print character command
 .rep:
