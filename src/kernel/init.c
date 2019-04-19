d8a2da331107c0da26a667a5b4ee8b4998c26563
Plaligraphy <evanac21@gmail.com>
2019-04-19 06:17:21 -0700

    Fixed bugs

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -3,7 +3,7 @@ void initalize_kernel()
 {
  text_color = 15;
  term_init();
- kprint("Nitro\n");
+ kprint("Nitro\n\0");
  //kprint("Initalizing at 0x01000");
  //kprint("C initalized.. Code: 0x0a");
  kprint("Testing");
