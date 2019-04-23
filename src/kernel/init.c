8de1e46e7adfe142686baed100c8b826ad3611e3
Plaligraphy <evanac21@gmail.com>
2019-04-22 20:39:58 -0700

    Bugs added, cant convert int to char*

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,13 +1,18 @@
 #include "headers/kernel.h"
 #include "headers/panic.h"
+#include <stdio.h>	
 void initalize_kernel()
 {
  
  text_color = 15;
  term_init();
  kprint("> ");
- term_init();
- kprint("> ");
- term_init();
- kprint("> ");
+ int cr2_var;
+ asm volatile ( 
+	"movq %%cr2, %%0;"
+	: "=r" (cr2_var)
+ );
+ char *dat = "";
+ sprintf(dat, cr2_var);
+ kprint(dat);
 }
