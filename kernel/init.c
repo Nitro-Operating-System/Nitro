0ba867b920cedf3d96076fc72fb078788b29baf6
Plaligraphy <evanac21@gmail.com>
2019-04-23 15:15:06 -0700

    Reworked dirs

---
+++ kernel/init.c
@@ -0,0 +1,20 @@
+#include "headers/kernel.h"
+#include "headers/panic.h"
+#include "headers/irh.h"
+#include <stdio.h>	
+void timeout(int delay) {
+int c, d;
+   
+   for (c = 1; c <= delay; c++)
+       for (d = 1; d <= delay; d++)
+       {}
+}
+void initalize_kernel()
+{ 
+ text_color = 10;//Light green
+ term_init(); 
+ kprint("NOS\n");
+ interrupt_handler();
+ timeout(10000);
+ onPanic("Application Quit!");
+}
---
+++ kernel/intr/interrupt.c
@@ -0,0 +1,6 @@
+#include "../headers/kernel.h"
+#include "../headers/irh.h"
+void interrupt_handler() 
+{
+ kprint("Interrupt Handler Started\n ");
+}
